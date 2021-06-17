726a0c9bc7939270df6aafb9428619d840d8ca7c
BroskinoLamer <62884274+BroskinoLamer@users.noreply.github.com>
2021-06-17 14:30:32 +0200

    Update DartFirstExcerise.dart

--- DartFirstExcerise.dart
+++ DartFirstExcerise.dart
@@ -1,3 +1,4 @@
+//Esericizio 1
 void main(){
   double base=20.0;
   double altezza = 20.0;
@@ -9,4 +10,22 @@ double calcolaArea(double base_della_funzione,double altezza_della_funzione){
   double area_rettangolo;
   area_rettangolo=base_della_funzione*altezza_della_funzione;
   return area_rettangolo;
+}
+
+//Esercizio 2
+void main(){
+  double base=20.0;
+  double altezza = 20.0;
+  double risultato=calcolaArea(base,altezza, true);
+  String risultato_in_Stringa=risultato.toString();
+  print("Il risultato è $risultato_in_Stringa");
+}
+double calcolaArea(double base_della_funzione,double altezza_della_funzione,bool isTriangolo){
+  double area; 
+  if(isTriangolo){//isTriangolo è già true rimuovo il confronto
+    area=base_della_funzione*altezza_della_funzione/2;
+  }else{
+    area=base_della_funzione*altezza_della_funzione; 
+  }
+  return area;
 }
